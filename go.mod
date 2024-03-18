972e695b97de91b466d1990f3c021d019e4bc3d5
Ari <aria.nit@hotmail.de>
2024-03-18 21:31:28 +0100

    godotenv package installed

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Ar1anit/Go-Web-Server
 
 go 1.22.1
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
 
-func main() {	
+	"github.com/joho/godotenv"
+)
+
+func main() {
 
 	fmt.Println("Hello, World!")
-}
 
+	godotenv.Load(".env")
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("PORT environment variable was not set")
+	}
+
+	fmt.Println("PORT is set to ", portString)
+}
